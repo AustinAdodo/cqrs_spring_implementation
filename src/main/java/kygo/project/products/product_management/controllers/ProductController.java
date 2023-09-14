faf3eed94dffebbc678cfc1936b74d0a1c9a52f4
Odia Adodo <subzbelow@gmail.com>
2023-09-14 16:38:34 +0100

    added productService to services

--- src/main/java/kygo/project/products/product_management/controllers/ProductController.java
+++ src/main/java/kygo/project/products/product_management/controllers/ProductController.java
@@ -8,10 +8,7 @@ import kygo.project.products.product_management.services.ProductQueryHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/products")
---
+++ src/main/java/kygo/project/products/product_management/services/ProductService.java
@@ -0,0 +1,28 @@
+package kygo.project.products.product_management.services;
+
+@Service
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
+}
+
