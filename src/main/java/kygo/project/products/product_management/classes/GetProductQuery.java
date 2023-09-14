0063677b8f62c2e884f0d758951416feb540af50
Odia Adodo <subzbelow@gmail.com>
2023-09-14 15:58:27 +0100

    Optimized ProductQuery.java

--- src/main/java/kygo/project/products/product_management/classes/GetProductQuery.java
+++ src/main/java/kygo/project/products/product_management/classes/GetProductQuery.java
@@ -3,5 +3,11 @@ package kygo.project.products.product_management.classes;
 public class GetProductQuery {
     private Long productId;
 
-    public Long getProductId(){return this.productId;}
+    public Long getProductId() {
+        return this.productId;
+    }
+
+    public void setProductId(Long Id) {
+        this.productId = Id;
+    }
 }
