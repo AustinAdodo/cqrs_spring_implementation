b11e01b1d5c0ef57da842f89c212c0b001a3207b
Odia Adodo <subzbelow@gmail.com>
2023-09-14 15:32:17 +0100

    configured @EnableJpaRepositories annotation to enable Spring Data JPA repositories

--- src/main/java/kygo/project/products/product_management/ProductManagementApplication.java
+++ src/main/java/kygo/project/products/product_management/ProductManagementApplication.java
@@ -1,9 +1,16 @@
 package kygo.project.products.product_management;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import java.util.List;
+
+/**
+ * I added the @EnableJpaRepositories annotation to enable Spring Data JPA repositories to ensure
+ * @see <a href="https://example.com/docs">Official Documentation</a> or {@link #}
+ */
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "")
 public class ProductManagementApplication {
 
     public static void main(String[] args) {
@@ -11,3 +18,4 @@ public class ProductManagementApplication {
     }
 
 }
+
