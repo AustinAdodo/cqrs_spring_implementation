a4d7739f52820f756c763ea348fcfd7d1478a891
Odia Adodo <subzbelow@gmail.com>
2023-09-15 18:09:19 +0100

    Defined ProductManagementApplication

--- src/main/java/kygo/project/products/product_management/ProductManagementApplication.java
+++ src/main/java/kygo/project/products/product_management/ProductManagementApplication.java
@@ -1,15 +1,19 @@
 package kygo.project.products.product_management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import java.util.List;
 
 /**
+ * NB: Ensure that your Spring Boot application class (the class with @SpringBootApplication annotation) or your
+ * Spring application configuration class scans the package containing your ProductRepository, ProductService,
+ * and other Spring components. You can do this by using the @ComponentScan annotation
  * The @EnableJpaRepositories annotation exists to enable Spring Data JPA repositories ensure CQRS
  * @see <a href="https://example.com/docs">Official Documentation</a> or {@link #}
  */
 @SpringBootApplication
+//@ComponentScan(basePackages = "kygo.project.products.product_management")
 @EnableJpaRepositories(basePackages = "kygo.project.products.product_management")
 public class ProductManagementApplication {
 
