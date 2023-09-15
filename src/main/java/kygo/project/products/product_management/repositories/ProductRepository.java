ff27fdee291f516530563de94cd31e23269231ce
Odia Adodo <subzbelow@gmail.com>
2023-09-15 18:01:02 +0100

    Defined ProductRepository.java class and added Documentation.

--- src/main/java/kygo/project/products/product_management/repositories/ProductRepository.java
+++ src/main/java/kygo/project/products/product_management/repositories/ProductRepository.java
@@ -2,7 +2,13 @@ package kygo.project.products.product_management.repositories;
 
 import kygo.project.products.product_management.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+/**
+ * Note: It's a good practice to annotate your repository interface with @Repository
+ * @see <a href="https://example.com/docs">Official Documentation</a> or {@link #}
+ */
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     // You can add custom query methods here if needed
 }
