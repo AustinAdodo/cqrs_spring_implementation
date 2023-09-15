cd244cb5734aaa2a011c5b4e14e6b820a17903e4
Odia Adodo <subzbelow@gmail.com>
2023-09-15 21:50:56 +0100

    Optimized GetProductQuery.java

--- src/main/java/kygo/project/products/product_management/classes/GetProductQuery.java
+++ src/main/java/kygo/project/products/product_management/classes/GetProductQuery.java
@@ -6,29 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Retrieval Business Logic.
- * Product Repository is injected in constructor to ensure dependency Injection.
+ * class not marked as bean because ..........
  * CQRS is ensured when the business logic for decoupling both persistence and retrieval are not linked.
+ *
  * @see <a href="https://example.com/docs">Official Documentation</a> or {@link #}
  */
 public class GetProductQuery {
     private Long productId;
-    private final ProductService service;
-    private final Product product;
 
-
-    @Autowired
-    public GetProductQuery(ProductService service, Product product) {
-        this.product = product;
-        this.service = service;
-    }
-
-    @Autowired
     public GetProductQuery(Long productId) {
-        this.product = service.getProductById(productId);
+        this.productId = productId;
     }
-//    public GetProductQuery (Long id) {
-//        this.productId = id;
-//    }
 
     public Long getProductId() {
         return this.productId;
