fdf12c7de4acabfe4ca6bb65dd7b7641200599a3
Odia Adodo <subzbelow@gmail.com>
2023-09-15 17:47:46 +0100

    Added documentation on ProductRepository Class.

--- src/main/java/kygo/project/products/product_management/classes/GetProductQuery.java
+++ src/main/java/kygo/project/products/product_management/classes/GetProductQuery.java
@@ -2,6 +2,7 @@ package kygo.project.products.product_management.classes;
 
 /**
  * Retrieval Business Logic.
+ * Product Repository is injected in constructor to ensure dependency Injection.
  * CQRS is ensured when the business logic for decoupling both persistence and retrieval are not linked.
  * @see <a href="https://example.com/docs">Official Documentation</a> or {@link #}
  */
